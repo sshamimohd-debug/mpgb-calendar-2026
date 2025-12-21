5321b8f775069f6fd52c3e195286973bf01cd2e8
sshamimohd-debug <sshamimohd@gmail.com>
2025-12-21 12:36:23 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,32 @@
-const CACHE_NAME = "mpgb-calendar-2026-v4";
+const CACHE_NAME = "mpgb-calendar-2026-v5";
 
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./service-worker.js",
   "./header_v2.png",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });
-
-
